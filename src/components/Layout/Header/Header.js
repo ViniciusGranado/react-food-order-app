a567bec60602dcffd962eabeefc8f378d48012b7
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-08-27 18:24:22 -0300

    Added Header component

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import mealsImage from '../../../assets/meals.jpg';
+import styles from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+
+        <button>Cart</button>
+      </header>
+
+      <div className={styles['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </>
+  );
+}
