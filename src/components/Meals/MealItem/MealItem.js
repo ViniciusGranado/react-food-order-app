e8159ec01430373b87e0c8f624e145cbbea78835
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-09-28 23:16:47 -0300

    Added refs and forward refs to project

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MealItemForm } from './MealItemForm/MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 import styles from './MealItem.module.css';
 
 export const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={styles.meal}>
@@ -15,7 +27,7 @@ export const MealItem = (props) => {
       </div>
 
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
